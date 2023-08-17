a490c15c26685947178a831380bfb0243f6452cc
Elkin Garcia <elkingarcia.11@gmail.com>
2023-08-17 10:34:56 -0400

    udpated docker file

--- src/pages/index.js
+++ src/pages/index.js
@@ -71,7 +71,6 @@ const apartments = [
     imageUrl: '../../public/aquaSix'
   },
 ];
-const apiKey = process.env.GOOGLE_MAPS_API_KEY
 export default function Home() {
   
   const [filteredApartmentList, setFilteredApartmentList] = useState(apartments);
