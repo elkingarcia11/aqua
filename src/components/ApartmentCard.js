6a9d0b01c7e5093a6bfef7d58f898fbc0d5ecd79
Elkin Garcia <elkingarcia.11@gmail.com>
2023-08-19 21:52:49 -0400

    updated apartment card documentation

--- src/components/ApartmentCard.js
+++ src/components/ApartmentCard.js
@@ -2,48 +2,63 @@ import React, { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
-
 import { Pagination, Navigation } from 'swiper';
 import { FaBed, FaBath } from "react-icons/fa";
-
-
 import { IoPeopleOutline } from "react-icons/io5";
 import styles from "@/styles/ApartmentCard.module.css";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
+/**
+ * ApartmentCard component displays information about an apartment,
+ * including images, details, and a reservation link.
+ *
+ * @param {number} aptId - The ID of the apartment.
+ * @param {number} sleeps - Number of people the apartment sleeps.
+ * @param {number} beds - Number of bedrooms in the apartment.
+ * @param {number} baths - Number of bathrooms in the apartment.
+ * @param {string} link - The reservation link for the apartment.
+ * @returns {JSX.Element} - Rendered ApartmentCard component.
+ */
 export default function ApartmentCard({ aptId, sleeps, beds, baths, link }) {
   const { t } = useTranslation();
   const [images, setImages] = useState([]);
+
   useEffect(() => {
     function importAll(r) {
+      /*
+        Goes through each module request string in the context directory, 
+        imports the corresponding modules, and returns an array of imported module objects
+      */
       return r.keys().map(r);
     }
 
     var imgs = [];
+
+    // Select import path based on aptId
     if (aptId === 1) {
-      imgs = importAll(require.context("src/assets/aquaOne/", false));
+      imgs = importAll(require.context(`src/assets/aqua1/`, false));
     } else if (aptId === 2) {
-      imgs = importAll(require.context("../assets/aquaTwo/", false));
+      imgs = importAll(require.context("../assets/aqua2/", false));
     } else if (aptId === 3) {
-      imgs = importAll(require.context("../assets/aquaThree/", false));
+      imgs = importAll(require.context("../assets/aqua3/", false));
     } else if (aptId === 4) {
-      imgs = importAll(require.context("../assets/aquaFour/", false));
+      imgs = importAll(require.context("../assets/aqua4/", false));
     } else if (aptId === 5) {
-      imgs = importAll(require.context("../assets/aquaFive/", false));
+      imgs = importAll(require.context("../assets/aqua5/", false));
     } else if (aptId === 6) {
-      imgs = importAll(require.context("../assets/aquaSix/", false));
+      imgs = importAll(require.context("../assets/aqua6/", false));
     }
-    let newArray = [];
-    imgs.forEach((imageUrl) => {
-      newArray.push(imageUrl.default);
-    });
-    setImages(newArray);
+
+    // Map image URLs to their default URLs and update the state directly
+    setImages(imgs.map(imageUrl => imageUrl.default));
   }, [aptId]);
 
   return (
     <div className={`${styles.swiperContainer}`}>
+
+      {/* Swiper component for image slider */}
       <Swiper
         cssMode={true}
         loop
@@ -60,16 +75,21 @@ export default function ApartmentCard({ aptId, sleeps, beds, baths, link }) {
       >
         {images.map((image, index) => (
           <SwiperSlide key={index} className={`${styles.swiperSlide}`}>
+            {/* Lazy-loaded image */}
             <LazyLoadImage src={image.src} alt={image.src} loading="lazy" className={`${styles.swiperSlideImg}`}/>
           </SwiperSlide>
         ))}
       </Swiper>
+
+      {/* Apartment description and details */}
       <div className={`${styles.descriptionContainer}`}>
         <div className={`${styles.details}`}>
           <div>
             <div className={`${styles.apt}`}>AQUA {aptId}</div>
             <div className={`${styles.aptSub}`}>El Pueblito, Puerto Plata</div>
           </div>
+
+          {/* Sleeps, Beds, and Baths icons and values */}
           <div className={`${styles.detailsOne}`}>
             <div>
               <div className={`${styles.iconAndDetails}`}>
@@ -89,6 +109,7 @@ export default function ApartmentCard({ aptId, sleeps, beds, baths, link }) {
             </div>
           </div>
         </div>
+        {/* Reservation link */}
         <div className={`${styles.reserve}`}>
           <a href={link} className={`${styles.reserveButton}`}>
             {t("reserve")}
